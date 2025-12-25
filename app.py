1006beda8744cfb47cbb4c545599127b0c50c5f8
musab855 <musabasif5@gmail.com>
2025-12-25 17:56:12 +0530

    Remove unused piece of code from app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_routes import routes
 
 app = Flask(__name__)
-app.secret_key = 'my-very-long-random-string-that-is-hard-to-guess'
 app.register_blueprint(routes)
 
 if __name__ == "__main__":
